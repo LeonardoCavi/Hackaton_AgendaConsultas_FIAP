c5378439d21557878f4c54649ae5ff6476acc281
LeonardoCavi <oliveira.leoc@hotmail.com>
2024-07-30 20:31:50 -0300

    [Feat] - Config Inicial Azure Communication Service

---
+++ HealthMed.AgendaConsulta.Infra/Vendor/Entities/AzureComunicationServiceParameters.cs
@@ -0,0 +1,11 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace HealthMed.AgendaConsulta.Infra.Vendor.Entities
+{
+    [ExcludeFromCodeCoverage]
+    public class AzureComunicationServiceParameters
+    {
+        public string ConnectionString { get; set; }
+        public string Email { get; set; }
+    }
+}
---
+++ HealthMed.AgendaConsulta.Infra/Vendor/NotificationManager.cs
@@ -0,0 +1,51 @@
+﻿using Azure;
+using Azure.Communication.Email;
+using HealthMed.AgendaConsulta.Infra.Vendor.Entities;
+
+namespace HealthMed.AgendaConsulta.Infra.Vendor
+{
+    public class NotificationManager
+    {
+        private readonly AzureComunicationServiceParameters _parameters;
+
+        public NotificationManager(AzureComunicationServiceParameters parameters)
+        {
+            _parameters = parameters;
+        }
+
+        public async Task SendEmailNotification(string paciente, string prestador, string destinatario, DateTime dataAgendamento)
+        {
+            EmailClient _email = new EmailClient(_parameters.ConnectionString);
+
+            var titulo = "Health Med - Nova Consulta Agendada";
+            var corpo = @$"<html>
+                        <body>
+                            <p>Olá, Dr. {prestador}!</p>
+                            <p>Você tem uma nova consulta marcada!</p>
+                            <p>Paciente: {paciente}.</p>
+                            <p>Data e horário: {dateTime.Date.ToString("dd/MM/yyyy")} às {dataAgendamento.TimeOfDay.ToString(@"hh\:mm\:ss")}.</p>
+                        </body>
+                      </html>";
+            var remetente = _parameters.Email;
+            var dest = destinatario;
+
+            try
+            {
+                EmailContent emailContent = new EmailContent(titulo)
+                {
+                    Html = corpo
+                };
+                EmailMessage emailMessage = new EmailMessage(sender, dest, emailContent);
+
+                EmailSendOperation sendEmailOp = await _email.SendAsync(WaitUntil.Completed, emailMessage);
+                EmailSendResult resultado = sendEmailOp.Value;
+                //Log de Sucesso
+            }
+            catch (Exception ex)
+            {
+                //Log de Falha
+            }
+
+        }
+    }
+}
